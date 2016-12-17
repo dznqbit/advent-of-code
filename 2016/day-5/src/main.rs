1266f14966a838d2cbe91c1745721c956866dca8
Quinton Harris <quinton.harris@gmail.com>
2016-12-16 16:21:54 -0800

    Cinema ready

--- 2016/day-5/src/main.rs
+++ 2016/day-5/src/main.rs
@@ -16,73 +16,36 @@ fn check_first_five(hash:&[u8]) -> bool {
   first_five == 0
 }
 
-fn pt1_password(key:&str) -> String {
-  let mut md5 = Md5::new();
-  let mut password = String::new();
-  let mut indices = 3000000..;
+fn cool_hacker_s(pw:&[i32;8], pi:usize, pv:u8) -> String {
+  let mut s = String::new();
 
-  while password.len() < 1 {
-    let idx = indices.next().unwrap();
+  for (i, &c) in pw.iter().enumerate() { 
 
-    md5.reset();
-    md5.input(key.to_string().as_bytes());
-    md5.input(idx.to_string().as_bytes());
-
-    let mut hash = [0; 16];
-    md5.result(&mut hash);
-
-    let c = hash[2] & 0x0F;
-
-    if check_first_five(&hash) {
-      // Grab the 6th character in the hash.
-      // Bitwise AND out the 5th char, write the 6th out to password.
-      write!(&mut password, "{:x}", c).unwrap();
-      continue;
+    if pi == i {
+      s.push_str(format!("{:x}", pv).as_ref());
+    } else {
+      if c == -1 {
+        s.push('_');
+      } else {
+        s.push_str(format!("{:x}", c).as_ref());
+      }
     }
-
-    // Animation
-    mv(1, 2);
-    printw(format!(" santops> H4XING PT 1 {}{} ", key, idx).as_ref());
-    printw(&password);
-    printw(format!("{:x}", c).as_ref());
-
-    refresh();
   }
 
-  password.clone()
+  s
 }
 
-// sloppy, but hey we're still under the rule of 3s ;D
-fn pt2_password(key:&str) -> String {
+fn pt1_password_complete(p:&str)      -> bool { p.len() < 8 }
+fn pt2_password_complete(a:&[i32;8])  -> bool { for c in a { if *c == -1 { return false } }; true }
+
+fn hack(key:&str) -> (String, String) {
   let mut md5 = Md5::new();
-  let mut password:[i32;8] = [-1; 8]; // String::from("        ");
   let mut indices = 0..;
 
-  fn password_incomplete(a:&[i32;8]) -> bool {
-    for c in a { if *c == -1 { return true } }
-    false
-  }
+  let mut pt1_password = String::new();
+  let mut pt2_password:[i32;8] = [-1; 8];
 
-  fn cool_hacker_s(pw:&[i32;8], pi:usize, pv:u8) -> String {
-    let mut s = String::new();
-
-    for (i, &c) in pw.iter().enumerate() { 
-
-      if pi == i {
-        s.push_str(format!("{:x}", pv).as_ref());
-      } else {
-        if c == -1 {
-          s.push('_');
-        } else {
-          s.push_str(format!("{:x}", c).as_ref());
-        }
-      }
-    }
-
-    s
-  }
-
-  while password_incomplete(&password) {
+  while !(pt1_password_complete(&pt1_password) && pt2_password_complete(&pt2_password)) {
     let idx = indices.next().unwrap();
 
     md5.reset();
@@ -92,29 +55,44 @@ fn pt2_password(key:&str) -> String {
     let mut hash = [0; 16];
     md5.result(&mut hash);
 
-    let sixth_char    = (hash[2] & 0x0F) as usize;
+    let sixth_char    = hash[2] & 0x0F;
     let seventh_char  = hash[3] >> 4;
 
-
     if check_first_five(&hash) {
+      // For pt 1
+      // Grab the 6th character in the hash.
+      // Bitwise AND out the 5th char, write the 6th out to password.
+      write!(&mut pt1_password, "{:x}", sixth_char).unwrap();
+
+      // For pt 2
       // 6th character determines location (zero-based index).
       // 7th character determines contents.
-      if sixth_char < 8 && password[sixth_char] == -1 {
-        password[sixth_char] = seventh_char as i32;
+      if sixth_char < 8 && pt2_password[sixth_char as usize] == -1 {
+        pt2_password[sixth_char as usize] = seventh_char as i32;
       }
     }
 
     // Animation
-    let chs = cool_hacker_s(&password, sixth_char, seventh_char);
+
+    // Pt 1
+    mv(1, 2);
+    printw(format!(" santops> H4XING PT 1 {}{} ", key, idx).as_ref());
+    printw(&pt1_password);
+    printw(format!("{:x}", sixth_char).as_ref());
+
+    // Pt 2
+    let chs = cool_hacker_s(&pt2_password, sixth_char as usize, seventh_char);
     mv(2, 2);
     printw(format!(" santops> H4XING PT 2 {}{} ", key, idx).as_ref());
     printw(&chs);
+
     refresh();
   }
 
-  let mut out = String::new();
-  for c in &password { write!(&mut out, "{:x}", c).unwrap(); }
-  out.clone()
+  let mut pt2_out = String::new();
+  for c in &pt2_password { write!(&mut pt2_out, "{:x}", c).unwrap(); }
+
+  (pt1_password.clone(), pt2_out.clone())
 }
 
 fn main() {
@@ -126,11 +104,11 @@ fn main() {
 
   let key = input.trim().clone();
 
-  let pt1_password = pt1_password(&key);
-  let pt2_password = pt2_password(&key);
+  let (pt1_password, pt2_password) = hack(&key);
 
   endwin();
 
   println!("\nPt 1: {}", pt1_password);
-  println!("\nPt 2: {}", pt2_password);
+  println!("Pt 2: {}\n", pt2_password);
 }
+
