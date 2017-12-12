35eb4d94af962ad7cd35cffa78678849bfd3951d
dznqbit <quinton.harris@gmail.com>
2017-12-11 18:35:53 -0800

    Added Guardfile

---
+++ Guardfile
@@ -0,0 +1,67 @@
+# A sample Guardfile
+# More info at https://github.com/guard/guard#readme
+
+## Uncomment and set this to only include directories you want to watch
+# directories %w(app lib config test spec features) \
+#  .select{|d| Dir.exists?(d) ? d : UI.warning("Directory #{d} does not exist")}
+
+## Note: if you are using the `directories` clause above and you are not
+## watching the project directory ('.'), then you will want to move
+## the Guardfile to a watched dir and symlink it back, e.g.
+#
+#  $ mkdir config
+#  $ mv Guardfile config/
+#  $ ln -s config/Guardfile .
+#
+# and, you'll have to watch "config/Guardfile" instead of "Guardfile"
+
+# https://github.com/guard/guard/wiki/Create-a-guard
+require 'guard/plugin'
+
+# the double-colons below are *required* for inline Guards!!!
+
+module ::Guard
+  class AdventOfCode < Plugin
+    include ::Guard::UI
+
+    def initialize(options = {})
+      opts = options.dup
+      super(opts) # important to call + avoid passing options Guard doesn't understand
+    end
+
+    def run_all
+      for year in 2015..(Time.now.year) do
+        for day in 1..31 do
+          run_advent(year, day)
+        end
+      end
+    end
+
+    def run_on_modifications(paths)
+      for path in paths
+        md = /(\d{4})\/day-(\d+)/.match(path)
+        year, day = md[1..2]
+        run_advent year, day
+      end
+    end
+
+    private
+
+    def run_advent(year, day)
+      test = true
+      output = `./advent.rb #{test ? "-t" : ""} #{year}/#{day}`
+      # logger.info "#{year}/#{day} START #{test ? "TEST" : ""}"
+      output.split("\n").each { |s| logger.info "#{year}/#{day} #{s}\n" }
+    end
+
+    def logger
+      ::Guard::UI.logger
+    end
+  end
+end
+
+
+guard :advent_of_code do
+  watch(/^\d{4}\/day-\d+\/src\/(.*)\.rs$/)
+  watch(/^\d{4}\/day-\d+\.rb$/)
+end
--- advent.rb
+++ advent.rb
@@ -2,7 +2,10 @@
 
 def exit_with_error(err); STDERR.puts(err); exit; end
 
-arg0 = ARGV[0]
+options, args = ARGV.partition { |a| a.start_with?("-") }
+
+arg0 = args[0]
+test = options.include?("-t")
 
 if arg0.nil?
   STDERR.puts "Usage: #{$0} [<year_id>/]<day_id>"
@@ -19,7 +22,23 @@ end
 # Cleanup so you can call with either '7' or 'day-7'
 day = "day-#{day_id.gsub('day-','')}"
 
-# Check for directory.
+# Build Input Path
+input_filename = test ? "#{day}-sample.input" : "#{day}.input"
+
+input_paths = [
+  # Typically the input file will be child of the year directory.
+  File.join(year, input_filename),
+
+  # Legacy Rust implementations (2016) put sample input inside year/day directories.
+  File.join(year, day, input_filename)
+]
+
+unless relative_input_path = input_paths.find { |fp| File.exists?(fp) }
+  exit_with_error("Couldn't find input, checked: \n\t#{input_paths.join("\n\t")}")
+end
+
+absolute_input_path = File.expand_path(relative_input_path)
+
 if File.directory?(day_dir_path=File.join(year, day))
   unless File.exists?(File.join(day_dir_path, 'src', 'main.rs'))
      exit_with_error("Could not find src/main.rs inside #{day_dir_path} !")
@@ -29,18 +48,13 @@ if File.directory?(day_dir_path=File.join(year, day))
 
   begin
     Dir.chdir(day_dir_path)
-    input_file = "#{day}.input"
-    exit_with_error("Could not find #{day_dir_path}/#{input_file}") unless File.exists?(input_file)
-    exec "cat #{input_file} | cargo run"
+    exec "cat #{absolute_input_path} | cargo run --quiet"
   ensure
     Dir.chdir(home_directory)
   end
 else
-  input_path = File.join(year, "#{day}.input")
   code_path = File.join(year, "#{day}.rb")
-
-  exit_with_error("Could not find #{input_path}") unless File.exists?(input_path)
   exit_with_error("Could not find #{code_path}")  unless File.exists?(code_path)
 
-  exec "cat #{input_path} | ./#{code_path}"
+  exec "cat #{absolute_input_path} | ./#{code_path}"
 end
