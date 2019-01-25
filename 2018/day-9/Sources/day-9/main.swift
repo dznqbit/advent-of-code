1edf35ca4eb8f00c14111b72a9480dc8aad23de8
Quinton Harris <quinton.harris@carbonfive.com>
2019-01-25 14:27:46 -0800

    Day 9 Pt2 in Swift

--- 2018/day-9/Sources/day-9/main.swift
+++ 2018/day-9/Sources/day-9/main.swift
@@ -1,6 +1,7 @@
 // http://adventofcode.com/2018/day/9
 
 import Foundation
+import Darwin.ncurses
 
 typealias Marble = Int
 
@@ -32,6 +33,8 @@ class MarbleGame : CustomStringConvertible {
 
   init(playerCount: Int, maxMarbleValue: Int) {
     marbles = [0]
+    marbles.reserveCapacity(maxMarbleValue)
+
     playerScores = [:]
     nextMarbleValue = 1
     self.maxMarbleValue = maxMarbleValue
@@ -66,7 +69,6 @@ class MarbleGame : CustomStringConvertible {
     nextMarbleValue += 1
   }
 
-  var currentMarbleValue: Marble { return marbles[currentMarbleIndex] }
   var marbleCount: Int { return marbles.count }
   var active: Bool { return nextMarbleValue <= maxMarbleValue }
 
@@ -76,19 +78,25 @@ class MarbleGame : CustomStringConvertible {
       .map { (idx, v) in idx == currentMarbleIndex ? "(\(v))" : String(v) }
       .joined(separator: " ")
 
-    return "[\(player)] \(marbleList)"
+    return "{\(nextMarbleValue)/\(maxMarbleValue)} [\(player)\(playerCount)] \(marbleList)"
   }
 }
 
 if let line = readLine(), let (playerCount, maxMarbleValue) = parseGame(line) {
   let game = MarbleGame(playerCount: playerCount, maxMarbleValue: maxMarbleValue)
-
-  while game.active {
-    game.tick()
-  }
-
+  while game.active { game.tick() }
   if let (_, score) = game.playerScores.max(by: { a, b in a.value < b.value }) {
     print("Pt. 1: \(score)")
   }
-  print("Pt. 2: {}", "???")
+
+  let game2 = MarbleGame(playerCount: playerCount, maxMarbleValue: maxMarbleValue * 100)
+  while game2.active {
+    game2.tick()
+    if game2.nextMarbleValue % 1000 == 0 {
+      print("\(game2.nextMarbleValue)/\(game2.maxMarbleValue)")
+    }
+  }
+  if let (_, score) = game2.playerScores.max(by: { a, b in a.value < b.value }) {
+    print("Pt. 2: \(score)")
+  }
 }
