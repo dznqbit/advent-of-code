0ef7567f7a02c3ece7d325f51d91e0d89f539a38
Quinton Harris <quinton.harris@carbonfive.com>
2022-10-25 23:04:34 -1000

    2021/2 pt 2

--- 2021/day-02/main.py
+++ 2021/day-02/main.py
@@ -25,3 +25,19 @@ instructions = [parse_instruction(s) for s in input.splitlines()]
 position = reduce(next_position, instructions, { "d": 0, "h": 0 })
 
 print(f"Pt 1: {position['h'] * position['d']}")
+
+def next_position_pt2(position, instruction):
+  (direction, distance) = instruction
+
+  match direction:
+    case "forward":
+      return { "aim": position["aim"], "d": position["d"] + position["aim"] * distance, "h": position["h"] + distance }
+    case "down":
+      return { "aim": position["aim"] + distance, "d": position["d"], "h": position["h"] }
+    case "up":
+      return { "aim": position["aim"] - distance, "d": position["d"], "h": position["h"] }
+    case _:
+      raise f"Unexpected direction \"{direction}\"" 
+
+position_pt2 = reduce(next_position_pt2, instructions, { "aim": 0, "d": 0, "h": 0 })
+print(f"Pt 2: {position_pt2['h'] * position_pt2['d']}")
