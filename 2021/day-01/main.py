b833b94c21bde90cd0be5e373eb48749ceb7e1df
Quinton Harris <quinton.harris@carbonfive.com>
2022-10-25 22:45:09 -1000

    2021/1 pt2

--- 2021/day-01/main.py
+++ 2021/day-01/main.py
@@ -1,11 +1,15 @@
 # http://adventofcode.com/2021/day/1
+from curses import window
 from sys import __stdin__
 from functools import reduce
-from itertools import pairwise
+from itertools import pairwise, islice
 
 input = __stdin__.read()
 depth_readings = [int(x) for x in input.splitlines()]
 depth_reading_increases = filter(lambda x: x[0] < x[1], pairwise(depth_readings))
 count_of_depth_reading_increases = len(list(depth_reading_increases))
-
 print(f"Pt 1: {count_of_depth_reading_increases}")
+
+windowed_depth_readings = [sum(islice(depth_readings, i, i + 3)) for i in range(0, len(depth_readings) - 2)]
+windowed_depth_reading_increases = len(list(filter(lambda x: x[0] < x[1], pairwise(windowed_depth_readings))))
+print(f"Pt 2: {windowed_depth_reading_increases}")
